c768e4af7cc4d037ab0184d2b0c4f45f28d818d5
Petar Prokić <petar_prokic@yahoo.com>
2014-11-14 19:03:00 +0100

    Create Program4
    odradjen 4ti zadatak iz zbirke

---
+++ Zbirka C#/Program4/Program4/Program4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program4
+{
+    class Program4
+    {
+        //Zadatak #4: Napisati program kojim se za unetu vrednost promenljive x izracunava i stampa vrednost funkcije
+        static void Main(string[] args)
+        {
+            //Deklarisanje podataka
+            double x, y;
+
+            //Unos podatka x
+            Console.Write("Unesite vrednost za x: ");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            //Izracunavanje vrednosti funkcije
+            y = Math.Sqrt(x - 1) + 1 / (x + 1);
+
+            //Stampanje rezultata
+            Console.WriteLine("Dobijena vrednost funkcije je {0:F3}.", y);
+
+
+            Console.WriteLine("Kraj rada...");
+            Console.ReadLine();
+
+        }
+    }
+}
