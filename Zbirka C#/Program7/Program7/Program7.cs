1eb8dda079096542aecebea255b155a6bd9188c0
Petar Prokić <petar_prokic@yahoo.com>
2014-11-14 19:49:28 +0100

    new prog7

---
+++ Zbirka C#/Program7/Program7/Program7.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program7
+{
+    class Program7
+    {
+        //Napisati program koji izracunava vrednost funkcije
+        static void Main(string[] args)
+        {
+            //Deklarisanje podataka
+            double x, y, z, f;
+            //Unos podataka
+            Console.Write("Unesite vrednost za x: ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Unesite vrednost za y: ");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Unesite vrednost za z: ");
+            z = Convert.ToDouble(Console.ReadLine());
+
+            if (x != y)
+            {
+                //Izracunavanje vrednosti funkcije
+                f = (x + y) * (x + 2 * z) / (x - y);
+                
+                //Stampanje rezultata
+                Console.WriteLine("Dobijena vrednost funkcije je {0:F3}", f);
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
