c2f72c503434af62e606e45cc942bf568acd60b0
Petar Prokić <petar_prokic@yahoo.com>
2014-11-15 15:08:03 +0100

    added prog10

---
+++ Zbirka C#/Program10/Program10/Program10.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program10
+{
+    class Program10
+    {
+        //Napisati program kojim se utvrdjuje da li je uneti znak malo ili veliko slovo, cifra ili specijalan znak
+        static void Main(string[] args)
+        {
+            var retry = true;
+            while (retry)
+            {
+                retry = false;
+                //Unos znaka
+                Console.WriteLine("\nUnesite znak: ");
+                char ch = Convert.ToChar(Console.ReadLine());
+
+                //Ispitivanje vrste znaka
+                if (ch >= 'a' && ch <= 'z')
+                    Console.WriteLine("{0} je malo slovo.", ch);
+                else if (ch >= 'A' && ch <= 'Z')
+                    Console.WriteLine("{0} je veliko slovo.", ch);
+                else if (ch >= '0' && ch <= '9')
+                    Console.WriteLine("{0} je cifra.", ch);
+                else
+                    Console.WriteLine("{0} je specijalan znak.", ch);
+
+                retry = true;
+            }
+      
+        }
+    }
+}
--- Zbirka C#/Program9/Program9/Program.cs
+++ Zbirka C#/Program9/Program9/Program.cs
@@ -8,8 +8,27 @@ namespace Program9
 {
     class Program
     {
+        //Napisati program kojim se za unetu vrednost x izracunava y po formuli
         static void Main(string[] args)
         {
+            //deklarisanje podataka
+            double x, y;
+            //Unos podatka x
+            Console.WriteLine("Unesite vrednost za x: ");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            //Realizacija ugnjezdene if-else sekcije
+            if (x > 1)
+                y = Math.Exp(x);
+            else if (x < 0)
+                y = Math.Abs(x);
+            else
+                y = Math.Sqrt(Math.Pow(x, 3));
+
+            Console.WriteLine("Za unetu vrednost x = {0} dobija se y = {1:F3}.", x, y);
+
+            Console.WriteLine("Kraj rada...");
+            Console.ReadLine();
         }
     }
 }
